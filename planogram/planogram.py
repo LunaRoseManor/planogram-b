3acd58571c1c5ba59c86c3b7441438cc8f386945
LunaRoseManor <143431646+LunaRoseManor@users.noreply.github.com>
2026-09-05 12:13:00 +0100

    Generate config file using Python on first use

--- planogram/planogram.py
+++ planogram/planogram.py
@@ -6,6 +6,7 @@ import cards
 import titles
 import decklists
 import os
+from datetime import datetime
 
 """
 Retrieves global config data from the specified file
@@ -21,6 +22,56 @@ def get_config():
     config["path"] = PATH
     
     return config
+
+def generate_config_file():
+    PATH = "data/config.json"
+    DATA = {
+        "app_id": "planogram",
+        "prog": "Planogram",
+        "description": "Command line EDHREC for Netrunner",
+        "epilog": "",
+        "prefix_chars": "-",
+        "color": True,
+        "args": [
+            {
+                "flags": [
+                    "-r",
+                    "--retrieve"
+                ],
+                "action": "store_true",
+                "type": "str",
+                "nargs": 0,
+                "required": False,
+                "help": "Retrieves latest data for card types, cards, decklists, factions, formats, restrictions, sides and snapshots from NetrunnerDB's API."
+            },
+            {
+                "flags": [
+                    "-q",
+                    "--query"
+                ],
+                "action": "store",
+                "type": "str",
+                "nargs": 1,
+                "required": False,
+                "help": "Filters decklist input data using jsonquerylang syntax"
+            },
+            {
+                "flags": [
+                    "-f",
+                    "--format"
+                ],
+                "action": "store",
+                "type": "str",
+                "nargs": 1,
+                "required": False,
+                "help": "Choose which format decklist data is filtered from"
+            }
+        ]
+    }
+    
+    # Attempt to write the above data to a file
+    with open(PATH, 'w', encoding="utf-8") as f:
+        json.dump(DATA, f, indent=4)
 
 def get_args():
     CONFIG = get_config()
@@ -105,64 +156,111 @@ def get_args():
     
     return args
 
+def is_installed():
+    install_file_name = "install.json"
+    installed = False
+    
+    if os.path.exists(install_file_name):
+        installed = True
+    
+    return installed
+
+def install():
+    # Output to let the user know first time setup is being run
+    print("Installing...")
+    
+    # Installation requires variables to be established
+    install_file_name = "install.json"
+    data_folder_path = "data"
+    
+    # Cache information about the current install
+    # Once this file is cached, planogram should assume it's been installed
+    with open(install_file_name, 'w', encoding="utf-8") as f:
+        data = {
+            "installed": True,
+            "date": datetime.today().strftime('%Y-%m-%d')
+        }
+        json.dump(data, f)
+    
+    # Create the data folder if it doesn't already exist
+    if not os.path.isdir("data"):
+        print("NOTICE: Data folder doesn't exist.")
+        print("Creating...")
+        os.makedirs(data_folder_path)
+    
+    if not os.path.exists("data/config.json"):
+        generate_config_file()
+    
+    print("Installed!")
+    
+    return True
+
 def main():
-    # Retrieve argument data both from the file and user input
-    args = get_args()
+    # Check if first run setup is complete
+    installed = is_installed()
     
-    # Then, check which of the flags has been used
-    # The retrieve flag is checked for first, and is handled like a seperate program
-    if args["retrieve"]:
-        # The fetch command requires double verification because of how long it takes.
-        selection = input("Are you sure? This will take a lot of clicks! (Y/n): ")
-        success = False # TODO: Change back to false
+    if installed:
+        # Retrieve argument data both from the file and user input
+        args = get_args()
         
-        if selection == 'Y':
-            # Only attempt a NetrunnerDB fetch if the user agrees.
-            success = retrieve.fetch_all()
+        # Then, check which of the flags has been used
+        # The retrieve flag is checked for first, and is handled like a seperate program
+        if args["retrieve"]:
+            # The fetch command requires double verification because of how long it takes.
+            selection = input("Are you sure? This will take a lot of clicks! (Y/n): ")
+            success = False # TODO: Change back to false
             
-            # Extra user feedback based on how the fetch went
-            if success == True:
-                print("Caching...")
+            if selection == 'Y':
+                # Only attempt a NetrunnerDB fetch if the user agrees.
+                success = retrieve.fetch_all()
                 
-                decklists.cache_legal_decklists()
-                cards.cache_legality()
-                titles.cache_titles()
-                
-                print("Success!")
-            else:
-                print("Unsuccessful.")
-        elif selection != 'n':
-            print("error: selection can only be 'Y' or 'n'.")
+                # Extra user feedback based on how the fetch went
+                if success == True:
+                    print("Caching...")
+                    
+                    decklists.cache_legal_decklists()
+                    cards.cache_legality()
+                    titles.cache_titles()
+                    
+                    print("Success!")
+                else:
+                    print("Unsuccessful.")
+            elif selection != 'n':
+                print("error: selection can only be 'Y' or 'n'.")
+            
+            return
         
-        return
-    
-    # I added this output because some operations are quite slow
-    print("Please wait...")
-    
-    # Otherwise, the output data should be modified based on program flags
-    # The list of decklists searched starts with every single one being in the cache
-    filtered_decklists = decklists.get_all_decklists()
-    
-    # The query command is for raw json queries
-    # This should be deprecated at some stage
-    # It's only really for debugging purposes
-    if args["query"] != None:
-        query = args["query"]
-        filtered_decklists = jsonquery(filtered_decklists, query)
-    
-    # Allows users to search by format
-    # NOTE: The method won't use for multiple operands, same as above
-    if args["format"] != None:
-        filtered_decklists = decklists.get_decklists(args["format"])
-    
-    # Finally, the output should be displayed
-    # By default this is just a big list of top cards
-    output = list.list(filtered_decklists)
-    
-    with open("output_file.txt", 'w', encoding="utf-8") as f:
-        f.write(output)
-    
-    card_titles = titles.get_titles()
+        # I added this output because some operations are quite slow
+        print("Please wait...")
+        
+        # Otherwise, the output data should be modified based on program flags
+        # The list of decklists searched starts with every single one being in the cache
+        filtered_decklists = decklists.get_all_decklists()
+        
+        # The query command is for raw json queries
+        # This should be deprecated at some stage
+        # It's only really for debugging purposes
+        if args["query"] != None:
+            query = args["query"]
+            filtered_decklists = jsonquery(filtered_decklists, query)
+        
+        # Allows users to search by format
+        # NOTE: The method won't use for multiple operands, same as above
+        if args["format"] != None:
+            filtered_decklists = decklists.get_decklists(args["format"])
+        
+        # Finally, the output should be displayed
+        # By default this is just a big list of top cards
+        output = list.list(filtered_decklists)
+        
+        with open("output_file.txt", 'w', encoding="utf-8") as f:
+            f.write(output)
+        
+        card_titles = titles.get_titles()
+    else:
+        print("WARNING: First time setup hasn't been run")
+        
+        install()
 
 if __name__ == "__main__":
     main()
